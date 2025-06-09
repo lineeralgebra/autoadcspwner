7ed7f63548ee907d9a2f64621b797e38b413b0f2
lineeralgebra <94987429+lineeralgebra@users.noreply.github.com>
2025-06-08 21:37:23 -0400

    Update parser.py

--- utils/parser.py
+++ utils/parser.py
@@ -23,30 +23,27 @@ def parse_stdout_output(stdout_text):
     for line in lines:
         line = line.strip()
 
-        # CA name line example: "CA Name                             : sequel-DC-CA"
         if line.startswith("CA Name"):
             current_ca = line.split(":",1)[1].strip()
 
-        # Template name line example: "Template Name                       : UserAuthentication"
         if line.startswith("Template Name"):
             current_template = line.split(":",1)[1].strip()
 
-        # Vulnerabilities line example: "[!] Vulnerabilities"
         if "[!] Vulnerabilities" in line:
             vuln_type = None  # reset for new section
 
-        # Example vuln line: "  ESC1                              : Enrollee supplies subject and template allows client authentication."
         if line.startswith("ESC1"):
             vuln_type = "ESC1"
 
-        # If we collected all three, add to results
+        elif line.startswith("ESC4"):
+            vuln_type = "ESC4"
+
         if current_ca and current_template and vuln_type:
             results.append({
                 "ca_name": current_ca,
                 "template_name": current_template,
                 "vuln": vuln_type
             })
-            # reset vuln_type for next template
             vuln_type = None
 
     return results
